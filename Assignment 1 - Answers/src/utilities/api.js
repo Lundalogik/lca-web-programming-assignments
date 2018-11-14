9ca552cb4bd6fe21cf2335d9f4166accfb371aff
Tommy Lindh <tommy.lindh@lundalogik.se>
2018-11-14 08:13:15 -0500

    Changed units to metric

--- Assignment 1 - Answers/src/utilities/api.js
+++ Assignment 1 - Answers/src/utilities/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export class WeatherApi {
     constructor() {
         this.apiKey = 'a46d2e1466ef7785a8c42afe66323886';
-        this.url = `http://api.openweathermap.org/data/2.5/forecast?appid=${this.apiKey}`;
+        this.url = `http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${this.apiKey}`;
     }
 
     getForecast(city = '', country = '') {
--- Assignment 1/src/utilities/api.js
+++ Assignment 1/src/utilities/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export class WeatherApi {
     constructor() {
         this.apiKey = 'YOUR API KEY';
-        this.url = `http://api.openweathermap.org/data/2.5/forecast?appid=${this.apiKey}`;
+        this.url = `http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${this.apiKey}`;
     }
 
     getForecast(city = '', country = '') {
