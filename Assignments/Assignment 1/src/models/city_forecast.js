6321fea7ab0a3754d6170819126567c4ed36a631
Tommy Lindh <tommy.lindh@lundalogik.se>
2018-12-13 08:41:32 -0500

    The assignment should not have any code in the assignment

--- Assignments/Assignment 1/src/models/city_forecast.js
+++ Assignments/Assignment 1/src/models/city_forecast.js
@@ -4,27 +4,5 @@ import WeatherData from './weather_data';
 export default class CityForecast {
     constructor(data) {
         // Construct a CityForecast object to hold information about the city and a five day forecast
-        this.cityid = data.city.id;
-        this.cityname = data.city.name;
-        this.country = data.city.country;
-
-        this.forecast = (data.list
-            .filter((forecastitem,index) => index % 8 === 0) //only take every 8 forecastitem in the list
-            .map(forecastitem => new WeatherData(forecastitem))
-        );
-
-        // Example with bodies for the functions
-        // this.forecast = (data.list
-        //     .filter((forecastitem,index) =>
-        //     {
-        //         return index % 8 === 0
-        //     }) //only take every 8 forecastitem in the list
-        //     .map(forecastitem =>
-        //     {
-        //         return new WeatherData(forecastitem)
-        //     })
-        // );
-
-
     }
 }
